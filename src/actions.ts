41330d38f6140bd78b0380e72df0920dc5560b17
James Abbottsmith <james.abbottsmith@audinate.com>
2023-01-02 14:35:31 +1100

    Made selectors a seperate option

--- src/actions.ts
+++ src/actions.ts
@@ -12,6 +12,19 @@ export const setDeviceSubscriptionsMutation = gql`
 `
 
 export function generateActions(self: AudinateDanteModule): CompanionActionDefinitions {
+	const availableRxChannels = self.domain.devices?.flatMap((d) => {
+		return d.rxChannels.map((rxChannel) => ({
+			id: `${rxChannel.index}@${d.id}`,
+			label: `${rxChannel.name}@${d.name}`,
+		}))
+	})
+
+	// TODO Consider making this it's own field to avoid confusion for the user
+	const variableSelector = [1, 2, 3, 4].map((s) => ({
+		id: `rx-selector-${s}`,
+		label: `Selector #${s}`,
+	}))
+
 	return {
 		subscribeChannel: {
 			name: 'Subscribe Dante Channel',
@@ -21,14 +34,30 @@ export function generateActions(self: AudinateDanteModule): CompanionActionDefin
 					type: 'dropdown',
 					label: 'Rx Channel@Device',
 					default: 'Select a receive channel',
-					choices: self.domain.devices?.flatMap((d) => {
-						return d.rxChannels.map((rxChannel) => ({
-							id: `${rxChannel.index}@${d.id}`,
-							label: `${rxChannel.name}@${d.name}`,
-						}))
-					}),
+					choices: availableRxChannels,
 					allowCustom: true,
 					tooltip: 'The receiving channel to set the subscription on',
+					isVisible: (o) => {
+						return o['useSelector'].valueOf() === false
+					},
+				},
+				{
+					id: 'rxSelector',
+					type: 'dropdown',
+					label: 'Rx Selector',
+					default: 'rx-selector-1',
+					choices: variableSelector,
+					tooltip: 'Use in combination with "set destination" actions',
+					isVisible: (o) => {
+						return o['useSelector'].valueOf() === true
+					},
+				},
+				{
+					id: 'useSelector',
+					type: 'checkbox',
+					label: 'Use Rx Selector',
+					default: false,
+					tooltip: 'Use in combination with "set destination" actions',
 				},
 				{
 					id: 'tx',
@@ -46,7 +75,14 @@ export function generateActions(self: AudinateDanteModule): CompanionActionDefin
 				},
 			],
 			callback: async (action) => {
-				const { rx, tx } = action.options
+				let { rx, tx, useSelector, rxSelector } = action.options
+
+				if (useSelector) {
+					if (!rxSelector || typeof rxSelector !== 'string') {
+						return
+					}
+					rx = self.getVariableValue(rxSelector)
+				}
 
 				if (!rx || typeof rx !== 'string') {
 					return
@@ -91,6 +127,14 @@ export function generateActions(self: AudinateDanteModule): CompanionActionDefin
 			name: 'Set Destination',
 			options: [
 				{
+					id: 'rxSelector',
+					type: 'dropdown',
+					label: 'Rx Selector',
+					default: 'rx-selector-1',
+					choices: variableSelector,
+					tooltip: 'The selector to set',
+				},
+				{
 					id: 'rx',
 					type: 'dropdown',
 					label: 'Rx Channel@Device',
@@ -106,7 +150,8 @@ export function generateActions(self: AudinateDanteModule): CompanionActionDefin
 				},
 			],
 			callback: async (action) => {
-				self.variables['rx-selector-1'] = action.options['rx'].toString()
+				const { rx, rxSelector } = action.options
+				self.variables[rxSelector.toString()] = rx.toString()
 				self.setVariableValues(self.variables)
 			},
 		},
