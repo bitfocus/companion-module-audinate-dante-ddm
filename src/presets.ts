fd81d04a1e00189c829947981d6ebbb02ed28296
James Abbottsmith <james.abbottsmith@audinate.com>
2023-01-03 10:13:33 +1100

    Created presets

--- src/presets.ts
+++ src/presets.ts
@@ -6,45 +6,86 @@ export function generatePresets(self: AudinateDanteModule): CompanionPresetDefin
 		subscribeChannel: {
 			name: 'Subscribe Dante Channel',
 			category: 'Commands',
-			feedbacks: [],
+			feedbacks: [
+				{
+					feedbackId: 'isSubscribed',
+					options: {
+						useSelector: false,
+						rxSelector: 'rx-selector-1',
+					},
+					style: {
+						bgcolor: parseInt('BBBB00', 16),
+					},
+				},
+				{
+					feedbackId: 'isSubscribedAndHealthy',
+					options: {
+						useSelector: false,
+						rxSelector: 'rx-selector-1',
+					},
+					style: {
+						bgcolor: parseInt('00CC00', 16),
+					},
+				},
+			],
 			type: 'button',
 			style: {
 				text: 'Subscribe Dante Channel',
 				size: '14',
-				color: parseInt('000000', 16),
-				bgcolor: parseInt('00CC00', 16),
+				color: parseInt('FFFFFF', 16),
+				bgcolor: parseInt('000000', 16),
 			},
-			steps: [],
-			// options: [
-			// 	{
-			// 		id: 'rx',
-			// 		type: 'dropdown',
-			// 		label: 'Rx Channel@Device',
-			// 		default: 'Select a receive channel',
-			// 		choices: self.domain.devices?.flatMap((d) => {
-			// 			return d.rxChannels.map((rxChannel) => ({
-			// 				id: `${rxChannel.index}@${d.id}`,
-			// 				label: `${rxChannel.name}@${d.name}`,
-			// 			}))
-			// 		}),
-			// 		allowCustom: true,
-			// 		tooltip: 'The receiving channel to set the subscription on',
-			// 	},
-			// 	{
-			// 		id: 'tx',
-			// 		type: 'dropdown',
-			// 		label: 'Tx Channel@Device',
-			// 		default: 'Select a transmit channel',
-			// 		choices: self.domain.devices?.flatMap((d) => {
-			// 			return d.txChannels.map((txChannel) => ({
-			// 				id: `${txChannel.name}@${d.name}`,
-			// 				label: `${txChannel.name}@${d.name}`,
-			// 			}))
-			// 		}),
-			// 		allowCustom: true,
-			// 		tooltip: 'The transmitting device to subscribe to',
-			// 	},
-			// ],
+			steps: [
+				{
+					up: [
+						{
+							actionId: 'subscribeChannel',
+							options: {
+								useSelector: false,
+								rxSelector: 'rx-selector-1',
+							},
+						},
+					],
+					down: [],
+				},
+			],
+		},
+		setRxSelector: {
+			name: 'Set Rx Selector',
+			category: 'Commands',
+			feedbacks: [
+				{
+					feedbackId: 'isSelected',
+					options: {
+						useSelector: false,
+						rxSelector: 'rx-selector-1',
+					},
+					style: {
+						bgcolor: parseInt('555555', 16),
+					},
+				},
+			],
+			type: 'button',
+			style: {
+				text: 'Set Rx Selector',
+				size: '14',
+				color: parseInt('FFFFFF', 16),
+				bgcolor: parseInt('000000', 16),
+			},
+			steps: [
+				{
+					up: [
+						{
+							actionId: 'setDestinationChannel',
+							options: {
+								useSelector: false,
+								rxSelector: 'rx-selector-1',
+							},
+						},
+					],
+					down: [],
+				},
+			],
 		},
 	}
 }
