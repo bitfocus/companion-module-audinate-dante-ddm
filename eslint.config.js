00b8fd6441396bf750142c06cd45e657f428c6b2
James Abbottsmith <james.abbottsmith@audinate.com>
2025-02-18 15:27:19 +1100

    Convert to ESLint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
-module.exports = [
-	{
-        parser: "@typescript-eslint/parser",
-        rules: {
-          'no-plusplus': ['error', { allowForLoopAfterthoughts: true }]
-        },
-		files: ['**/*.ts'],
-	},
-]
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    },
+  },
+  {
+    rules: {
+      ...tsPlugin.configs.recommended.rules, // Include @typescript-eslint recommended rules
+    },
+  },
+];
